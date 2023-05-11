3a20ffcb808c286a59f816facfb9ccf5448ef784
Denis <tarvusvarron97@gmail.com>
2023-05-11 03:08:56 +0300

    fix

--- Exams/Module8ExamTask4/Program.cs
+++ Exams/Module8ExamTask4/Program.cs
@@ -3,38 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-[Serializable]
-public class Student
+namespace FinalTask
 {
-    public string Name { get; set; }
-    public string Group { get; set; }
-    public DateTime DateOfBirth { get; set; }
-}
-
-class Program
-{
-    static void Main(string[] args)
+    [Serializable]
+    public class Student
     {
-        string path = @"D:\\students.bin";
-        var students = ReadFromBinaryFile<List<Student>>(path);
-
-        string directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Students");
-        Directory.CreateDirectory(directoryPath);
-
-        foreach (var student in students)
-        {
-            string filePath = Path.Combine(directoryPath, $"{student.Group}.txt");
-            string studentData = $"{student.Name}, {student.DateOfBirth.ToString("dd.MM.yyyy")}";
-            File.AppendAllText(filePath, studentData + Environment.NewLine);
-        }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 
-    static T ReadFromBinaryFile<T>(string filePath)
+    class Program
     {
-        using (Stream stream = File.Open(filePath, FileMode.Open))
+        static void Main(string[] args)
         {
-            var binaryFormatter = new BinaryFormatter();
-            return (T)binaryFormatter.Deserialize(stream);
+            string path = @"D:\\students.bin";
+            var students = ReadFromBinaryFile<List<Student>>(path);
+
+            string directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Students");
+            Directory.CreateDirectory(directoryPath);
+
+            foreach (var student in students)
+            {
+                string filePath = Path.Combine(directoryPath, $"{student.Group}.txt");
+                string studentData = $"{student.Name}, {student.DateOfBirth.ToString("dd.MM.yyyy")}";
+                File.AppendAllText(filePath, studentData + Environment.NewLine);
+            }
+        }
+
+        static T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
         }
     }
 }
