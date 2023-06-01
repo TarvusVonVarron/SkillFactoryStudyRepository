db7a1c6db5c2571c17d6ad3ab0e9f76330669ec8
Denis <tarvusvarron97@gmail.com>
2023-06-02 01:25:17 +0300

    Exams 10 and 9

---
+++ Exams/Module10Exam/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public interface ICalculator
+{
+    double Add(double a, double b);
+}
+
+public class Calculator : ICalculator
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+public interface ILogger
+{
+    void LogError(string message);
+    void LogInfo(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void LogError(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    public void LogInfo(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICalculator calculator = new Calculator();
+        ILogger logger = new ConsoleLogger();
+
+        try
+        {
+            Console.Write("Введите первое число: ");
+            double a = Double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double b = Double.Parse(Console.ReadLine());
+
+            double result = calculator.Add(a, b);
+
+            logger.LogInfo($"Сумма чисел: {result}");
+        }
+        catch (FormatException)
+        {
+            logger.LogError("Введено некорректное значение. Пожалуйста, убедитесь, что вы вводите целое число.");
+        }
+        finally
+        {
+            logger.LogInfo("Калькулятор завершил работу.");
+        }
+    }
+}
---
+++ Exams/Module9Exam/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class InvalidSortOptionException : Exception
+{
+    public InvalidSortOptionException(string message) : base(message) { }
+}
+
+public class NameSorter
+{
+    public event Action<List<string>> SortAscending;
+    public event Action<List<string>> SortDescending;
+
+    private List<string> names;
+
+    public NameSorter(List<string> names)
+    {
+        this.names = names;
+
+        SortAscending += list => list.Sort();
+        SortDescending += list => list.Sort((x, y) => y.CompareTo(x));
+    }
+
+    public void Sort(int option)
+    {
+        switch (option)
+        {
+            case 1:
+                SortAscending?.Invoke(names);
+                break;
+            case 2:
+                SortDescending?.Invoke(names);
+                break;
+            default:
+                throw new InvalidSortOptionException("Неверный тип сортировки. Введите 1 (по возрастанию) или 2 (по убыванию).");
+        }
+    }
+
+    public override string ToString()
+    {
+        return string.Join(", ", names);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> names = new List<string> { "Иванов", "Петров", "Сидоров", "Кузнецов", "Зайцев" };
+
+        NameSorter nameSorter = new NameSorter(names);
+
+        while (true)
+        {
+            Console.WriteLine("Введите 1 для сортировки по возрастанию, 2 для сортировки по убыванию:");
+
+            try
+            {
+                int option = Int32.Parse(Console.ReadLine());
+                nameSorter.Sort(option);
+                Console.WriteLine(nameSorter);
+            }
+            catch (InvalidSortOptionException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный ввод. Пожалуйста, введите число.");
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
